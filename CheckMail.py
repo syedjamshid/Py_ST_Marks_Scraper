f1698f3b7105c90a7f1416061feac2d2b3e69cb6
Karthi-Villain <69628712+Karthi-Villain@users.noreply.github.com>
2023-02-07 00:11:35 +0530

    Update CheckMail.py

--- CheckMail.py
+++ CheckMail.py
@@ -1,21 +1,12 @@
 import smtplib
 
-def CheckSmtpServer(Host, Port):
+def CheckSmtpServer():
     try:
-        Smtp = smtplib.SMTP(Host, Port)
+        Smtp = smtplib.SMTP(os.getenv('MServer',default='smtp-mail.outlook.com'),os.getenv('MPort',default=587))
         Smtp.ehlo()
         Smtp.quit()
         return True
     except:
         return False
-
-# Example usage
-Host = "smtp-mail.outlook.com"
-Port = 587
-
-if CheckSmtpServer(Host, Port):
-    print("SMTP server is accessible")
-else:
-    print("SMTP server is not accessible")
 
 #Lol, This is Generated with ChatGPT
