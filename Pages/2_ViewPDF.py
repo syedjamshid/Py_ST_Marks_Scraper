fdab17eaba29f24e120eb75b8d861687fd665fe3
Karthi-Villain <69628712+Karthi-Villain@users.noreply.github.com>
2023-02-06 11:24:55 +0530

    Add files via upload

---
+++ Pages/2_ViewPDF.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import base64, time, os
+#--Session Initilization--
+if 'PDF_Name' not in st.session_state:
+    st.session_state['PDF_Name'] = ''
+if 'StudName' not in st.session_state:
+    st.session_state['StudName'] = ''
+
+st.title('PDF Tool')
+if st.session_state['StudName'] !='' and st.session_state['PDF_Name'] !='':
+    st.write(st.session_state['StudName']+' Your PDF is Here')
+    PDF_Name=st.session_state['PDF_Name']
+    with open(PDF_Name, "rb") as Pdf_file:
+        PDFbytes = Pdf_file.read()
+    c1,c2,c3=st.columns(3)
+    with c2:
+        st.download_button(label="Download PDF", key='3',
+            data=PDFbytes,
+            file_name=PDF_Name,
+            mime='application/octet-stream')
+    base64_Pdf = base64.b64encode(PDFbytes).decode('utf-8')
+    PDF_Display=f'<iframe src="data:application/pdf;base64,{base64_Pdf}" width="800" height="800" type="application/pdf"></iframe>'
+    st.markdown(PDF_Display, unsafe_allow_html=True)
+else:
+    st.error('Generate Your Results First')
+    time.sleep(3)
+    st.sidebar.error('Generate Your Results First')
---
+++ Pages/3_👻_Admin.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import os, shutil, logging
+
+st.title('🚫:red[Restricted]🚫')
+st.subheader(':blue[Only for Admins]')
+ParentDir=os.path.dirname(__file__).replace("Pages","")
+Dir=os.getenv('Dir',default='PDFs')
+col1,col2,col3=st.columns([1,1,1])
+with col2:
+    st.image(os.getenv('Avatar',default='https://github.com/Karthi-Villain/Py_Marks_Scraper/raw/main/Admin.png'),width=224)
+    Secret=st.text_input('Secret Key',placeholder='Enter the Secret Key:')
+
+    if Secret == os.getenv('Secret'):
+        logging.info('Login Successful')
+        st.success('Login Successful')
+        Clear_Data=st.button('Clear Data')
+        Log=open(ParentDir+'/Log.txt')
+    
+        st.download_button('Download Log File',
+                        data=Log.read(),
+                        file_name='Log.txt',
+                        mime='text/plain')
+        logging.info('Log file Exported Successful')
+        if Clear_Data:
+            try:
+                shutil.rmtree(ParentDir+Dir+'\\')
+                logging.info('Cleared Data Successfully - '+ParentDir+Dir)
+            except Exception as e:
+                logging.warning(e)
+    else:
+        st.error('You have Nothing to Do Here 🤪')
+
+
