9994b95b35ca70b0cd202bc4b9ef9ca047c0576d
Karthi-Villain <69628712+Karthi-Villain@users.noreply.github.com>
2023-02-07 09:44:03 +0530

    Update Results.py

--- Results.py
+++ Results.py
@@ -273,10 +273,10 @@ if submit_button:
 
             #--Mailing--
             if StudMail!='':
-                if CheckSmtpServer():
+                if CheckSmtpServer(os.getenv('MServer',default='smtp-mail.outlook.com'),os.getenv('MPort',default=587)):
                     logging.info("SMTP server is accessible."+os.getenv('MServer',default='smtp-mail.outlook.com')+'-'+str(os.getenv('MPort',default=587)))
                     st.write("You Will Recieve a Mail Shortly :smirk:")
                     SendMails(PrintMarks,StudName,StudMail,Roll)
 
                 else:
-                    logging.info("SMTP server is not accessible.")
+                    logging.info("SMTP server is not accessible."+os.getenv('MServer',default='smtp-mail.outlook.com')+'-'+str(os.getenv('MPort',default=587)))
