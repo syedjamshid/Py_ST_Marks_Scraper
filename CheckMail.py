a4babbc0b25c8c0a5297d2e9ad33bca374719651
Karthi-Villain <69628712+Karthi-Villain@users.noreply.github.com>
2023-02-07 09:45:27 +0530

    Update CheckMail.py

--- CheckMail.py
+++ CheckMail.py
@@ -1,9 +1,9 @@
 import smtplib
 
-def CheckSmtpServer():
+def CheckSmtpServer(Host, Port):
     try:
         logging.info('Checking SMTP') 
-        Smtp = smtplib.SMTP(os.getenv('MServer',default='smtp-mail.outlook.com'),os.getenv('MPort',default=587))
+        Smtp = smtplib.SMTP(Host,Port))
         Smtp.ehlo()
         Smtp.quit()
         return True
