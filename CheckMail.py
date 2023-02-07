c8a4cfba01c8e94378887b785b8dbf744e9abad6
Karthi-Villain <69628712+Karthi-Villain@users.noreply.github.com>
2023-02-07 10:03:40 +0530

    Update CheckMail.py

--- CheckMail.py
+++ CheckMail.py
@@ -3,7 +3,7 @@ import smtplib
 def CheckSmtpServer(Host, Port):
     try:
         logging.info('Checking SMTP') 
-        Smtp = smtplib.SMTP(Host,Port))
+        Smtp = smtplib.SMTP(Host,Port)
         Smtp.ehlo()
         Smtp.quit()
         return True
