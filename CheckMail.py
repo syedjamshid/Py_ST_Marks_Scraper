5e4752ac0d4f4e58a7bf4f5ca5c52e14cce91f3a
Karthi-Villain <69628712+Karthi-Villain@users.noreply.github.com>
2023-02-07 22:43:08 +0530

    Update CheckMail.py

--- CheckMail.py
+++ CheckMail.py
@@ -1,13 +1,13 @@
 import smtplib
-
+import logging
 def CheckSmtpServer(Host, Port):
     try:
         logging.info('Checking SMTP') 
-        Smtp = smtplib.SMTP(Host,Port)
+        Smtp = smtplib.SMTP(Host,int(Port))
         Smtp.ehlo()
         Smtp.quit()
-        return True
-    except:
+    except :
         return False
-
+    else:
+        return True
 #Lol, This is Generated with ChatGPT
